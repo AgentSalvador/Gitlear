e8457085ccc3689f51a6a208de0dc3a291a7550e
AgentSalvador <85904089+AgentSalvador@users.noreply.github.com>
2023-05-01 22:50:53 +0530

    Update Script1.sh

--- Script1.sh
+++ Script1.sh
@@ -9,5 +9,5 @@ echo "We are done with git"
 echo "This example is of branching strategy"
 echo "Hello Batch-16, We are learning Git/Github"
 echo "Good morning batch-18"
-echo "This is Siddharth's Commitls"
+echo "This is Siddharth's Commit"
 
